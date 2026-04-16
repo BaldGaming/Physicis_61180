df98ce91d61a453d4ee0ca31806eed745c13d1f8
Itay Rosen <itayrosen04@gmail.com>
2026-04-16 12:29:57 +0300

    week 3

---
+++ Week_3/const_F.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+F=float(input())
+v0=float(input())
+L=float(input())
+qn=int(input())
+
+m=1
+
+#### your code
+
+
+
+####
+#output
+Answers=[0,t_1,t_2,t_3]
+ans=Answers[qn]
+print(f'{ans:.3g}')
+
+
+
